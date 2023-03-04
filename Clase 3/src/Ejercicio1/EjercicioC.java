08c57665cb00114ec08f82ba2d0f05b17e63582b
AcostaF12 <107961278+AcostaF12@users.noreply.github.com>
2023-03-04 15:01:18 -0300

    Ejercicio C completado

--- Clase 3/src/Ejercicio1/EjercicioC.java
+++ Clase 3/src/Ejercicio1/EjercicioC.java
@@ -2,4 +2,25 @@ package Ejercicio1;
 
 public class EjercicioC {
     
+    public static void main(String[] args) {
+        
+        int[] numeros = new int[] {1, 3, 5, 10, 15, 25, 50, 100};
+
+        int sumaTotal = sumarNumerosMayoresA(5, numeros);
+
+        System.out.println("La suma total es de: " + sumaTotal);
+    }
+
+    public static int sumarNumerosMayoresA(int numeroUmbral, int[] numerosASumar) {
+
+    int sumaTotalHastaAhora = 0;
+
+    for (int i = 0; i < numerosASumar.length; i++) {
+        if (numerosASumar[i] > numeroUmbral) {
+            sumaTotalHastaAhora += numerosASumar[i];
+        }
+    }
+
+    return sumaTotalHastaAhora;
+    }
 }
