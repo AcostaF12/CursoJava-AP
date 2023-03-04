95dfc10b54376f1cfa98a10d24c273f7bb2b02a5
AcostaF12 <107961278+AcostaF12@users.noreply.github.com>
2023-03-04 14:34:57 -0300

    Ejercicio B terminado

--- Clase 3/src/Ejercicio1/EjercicioB.java
+++ Clase 3/src/Ejercicio1/EjercicioB.java
@@ -1,5 +1,32 @@
 package Ejercicio1;
 
+import java.util.Arrays;
+
 public class EjercicioB {
     
+    public static void main(String[] args) {
+        
+        int [] vectorResultante = ordenarVectorEnOrden(126, 35, 92, "descendente");
+
+        System.out.println(Arrays.toString(vectorResultante));
+    }
+
+    public static int[] ordenarVectorEnOrden(int numero1, int numero2, int numero3, String orden) {
+        
+        int [] vectorOrdenado = new int[] {numero1, numero2, numero3};
+        Arrays.sort(vectorOrdenado);
+
+        if (orden.equalsIgnoreCase("descendente")) {
+
+            int[] vectorInvertido = new int[vectorOrdenado.length];
+
+            for (int i = vectorOrdenado.length - 1, j = 0; i >= 0; i--, j++) {
+                vectorInvertido[j] = vectorOrdenado[i];
+            }
+
+            vectorOrdenado = vectorInvertido;
+        }
+
+        return vectorOrdenado;
+    }
 }
